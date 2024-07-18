2916a3cd0837ecdcd74d9321bb20b4c8b663c28a
abhi9ab <johannocap004@gmail.com>
2024-07-18 16:54:47 +0530

    mid2

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
             <h1 className='mb-5'>help</h1>
             <h1 className='text-gray-300'>get help</h1>
             <h1 className='text-gray-300'>status</h1>
-            <h1 className='text-gray-300'>contact us <span className='lowercase'>abhinabdas@gmail.com</span></h1>
+            <h1 className='text-gray-300'>contact us <span className='lowercase'>abhinabdas004@gmail.com</span></h1>
             <h1 className='text-gray-300'>contact us <a href='https://twitter.com/abhi9ab_' className='text-blue-500 hover:underline' target='_blank'>twitter</a></h1>
           </div>
           <div className='flex flex-col gap-2 w-[150px] text-left text-nowrap'>
